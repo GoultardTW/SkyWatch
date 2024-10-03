840ab4028b1109e8a5c2242cada72ce84a70767d
GoultardTW <pietragalla.1965270@studenti.uniroma1.it>
2024-10-03 12:42:12 +0200

    Path function X version

--- src/control_center/control_center.cpp
+++ src/control_center/control_center.cpp
@@ -20,51 +20,55 @@ std::string goTo(int x, int y, int nx, int ny){
     return res;
 }
 
-// Returns a path that passes through the rows 
-std::string goToGrid(int* x, int* y, int step){
+// Returns a path that passes through the rows
+std::string goToGridX(int* x, int* y, int step){
     std::string res = "";
-    int ax = *x;
-    int ay = *y;
+    int ax = *x, ay = *y;
     int temp = 0;
-    while (step > 0){
-        
-        int direction = ay % 2;
-        temp = (!direction) ? (299 - ax) : ax;
-        temp = std::min(temp, step);
+    bool goRight = false;
+
+    while(step>0){
+        goRight = (ay%2==0); // If it's even go to the right
+        temp = (goRight) ? (299 - ax) : ax; // Steps to reach the border
+        temp = std::min(temp, step); // Min between steps left and temp
 
         for (int k=0; k<temp; k++){
-            if(abs(ax-150)+abs(ay-150)==step){
+            if(step <= abs(ax-150)+abs(ay-150)){
                 res = res + goTo(ax, ay, 150, 150);
                 step = 0;
                 break;
             }
-            res = res + (!direction ? "r" : "l");
-            ax += (!direction ? 1 : -1);
+            if(goRight){
+                res = res + "r";
+                ax ++;
+            }else{
+                res = res + "l";
+                ax --;
+            }
             step -= 1;
         }
 
-        if (step > 0) {
-            if (ay < 299) {
-                if(abs(ax-150)+abs(ay-150)==step){
-                    res = res + goTo(ax, ay, 150, 150);
-                    step = 0;
-                    break;
-                }
-                res += 'd'; 
-                step -= 1; 
-                ay += 1; 
-            } else {
+        if(step>0 && ay<299){
+            if(step <= abs(ax-150)+abs(ay-150)){
+                res = res + goTo(ax, ay, 150, 150);
+                step = 0;
                 break;
             }
+            res += "d"; 
+            step -= 1; 
+            ay += 1; 
         }
     }
-    if(ax < 299 && (ay%2)==0){
+
+    goRight = (ay%2==0);
+    if(ax < 299 && goRight){
         ax++;
-    }else if (ax > 0 && (ay%2)==1){
+    }else if (ax > 0 && !goRight){
         ax--;
-    }else if (((ax==299) && (ay%2)==0 && (ay<299)) || ((ax==0) && (ay%2)==1 && (ay<299))){
+    }else if (((ax>=299) && goRight && (ay<299)) || ((ax<=0) && !goRight && (ay<299))){
         ay++;
     }
+
     *x = ax;
     *y = ay;
     return res;
@@ -80,15 +84,14 @@ void findPaths(std::vector<std::string>* paths) {
         step = 750;
         path = goTo(150, 150, x, y);
         step -= path.length();
-        path = path + goToGrid(&x, &y, step);
-        paths->push_back(path);
-        // incrementare di 1 x per il path successivo.
+        path = path + goToGridX(&x, &y, step);
+        paths->emplace_back(path);
     }
 
     if (x > 0) {
         path = goTo(150, 150, x, y);
         step = x;
-        path = goToGrid(&x, &y, step);
-        paths->push_back(path);
+        path = path + goToGridX(&x, &y, step);
+        paths->emplace_back(path);
     }
 }
